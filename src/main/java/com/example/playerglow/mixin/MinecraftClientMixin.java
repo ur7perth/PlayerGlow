0298424269e42ba7811ad579baf3cebea09e5f48
ur7perth <ur7.perth@gmail.com>
2026-09-18 20:06:24 +0300

    Create MinecraftClientMixin.java

---
+++ src/main/java/com/example/playerglow/mixin/MinecraftClientMixin.java
@@ -0,0 +1,20 @@
+Enterpackage com.example.playerglow.mixin;
+
+import com.example.playerglow.GlowManager;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+
+    @Inject(method = "hasOutline", at = @At("HEAD"), cancellable = true)
+    private void playerglow$hasOutline(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+        if (GlowManager.shouldGlow(entity)) {
+            cir.setReturnValue(true);
+        }
+    }
+}
